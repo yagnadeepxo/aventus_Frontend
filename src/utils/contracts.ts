d107f6bebfb69d6aaacfb4f61dd6a56eaa00a3dd
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2023-06-02 19:36:57 +0530

    chore: update contract address

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
 import tornadoJSON from "../json/Tornado.json";
 
-export const tornadoAddress = "0x4663086ed4A490Ea0f3bb1787E38AFFC417c5397"
+export const tornadoAddress = "0x6f08E7CCAac17a7089634D6bB763c1d6FaAC9F79"
 export const tornadoInterface = new ethers.utils.Interface(tornadoJSON);
