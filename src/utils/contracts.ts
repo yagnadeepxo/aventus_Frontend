2e072518e00212602bb0875bc20d0b809e966a82
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2023-06-02 20:42:22 +0530

    chore: add polygon address

--- src/utils/contracts.ts
+++ src/utils/contracts.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
 import tornadoJSON from "../json/Tornado.json";
 
-export const tornadoAddress = "0x6f08E7CCAac17a7089634D6bB763c1d6FaAC9F79"
+export const tornadoAddress = "0x5D86B054af1072219b3Be1852E40dd07436eeFC7"
 export const tornadoInterface = new ethers.utils.Interface(tornadoJSON);
